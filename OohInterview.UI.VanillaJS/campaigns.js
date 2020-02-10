e95f64a7229018039d9fe70d88277079ca917beb
Marc-André Beaudet-Racine <>
2020-02-09 23:21:00 -0500

    Add Vanilla JS UI

---
+++ OohInterview.UI.VanillaJS/campaigns.js
@@ -0,0 +1,19 @@
+function loadCampaigns () {
+    let request = new XMLHttpRequest();
+    request.open('GET', 'https://localhost:6001/api/v1/campaigns', true);
+
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            let data = JSON.parse(this.response);
+            data.items.forEach(campaign => {
+                const campaignElement = document.createElement('div');
+                campaignElement.textContent = campaign.name;
+
+                const campaignsContainer = document.getElementById('campaigns-container');
+                campaignsContainer.appendChild(campaignElement);
+            })
+        }
+    };
+
+    request.send();
+}
---
+++ OohInterview.UI.VanillaJS/navigationBar.js
@@ -0,0 +1,7 @@
+let html = '<div>\
+                <a href="./index.html">Home</a>\
+                <a href="./faces.html">Faces</a>\
+                <a href="./campaigns.html">Campaigns</a>\
+            </div>';
+
+document.getElementById('nav').innerHTML = html;
