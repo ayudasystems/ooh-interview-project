a9b333dac20b0e24f1ed71d27595a0d148ab0696
Tomas Morton <tomas@ayudasystems.com>
2020-02-10 14:58:24 -0500

    Remove Cancellation Tokens

--- OohInterview.Queries/Campaigns/List/IListCampaigns.cs
+++ OohInterview.Queries/Campaigns/List/IListCampaigns.cs
@@ -1,10 +1,7 @@
-using System.Threading;
-using System.Threading.Tasks;
-
 namespace OohInterview.Queries.Campaigns.List
 {
     public interface IListCampaigns
     {
-        ListCampaignsResult List(CancellationToken cancellationToken);
+        ListCampaignsResult List();
     }
 }
--- OohInterview.Queries/Faces/List/IListFaces.cs
+++ OohInterview.Queries/Faces/List/IListFaces.cs
@@ -1,10 +1,7 @@
-using System.Threading;
-using System.Threading.Tasks;
-
 namespace OohInterview.Queries.Faces.List
 {
     public interface IListFaces
     {
-        ListFacesResult List(CancellationToken cancellationToken);
+        ListFacesResult List();
     }
 }
