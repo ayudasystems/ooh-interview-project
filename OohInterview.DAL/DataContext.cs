ef4c6d8be285c1e4f2b2561e58123229ab93a401
Tomas Morton <tomas@ayudasystems.com>
2020-02-10 16:38:03 -0500

    Allow skipping DataContext initialisation
    Signed-off-by: Tomas Morton <tomas@ayudasystems.com>

--- OohInterview.DAL/DataContext.cs
+++ OohInterview.DAL/DataContext.cs
@@ -6,15 +6,16 @@ namespace OohInterview.DAL
 {
     public class DataContext
     {
-        public IList<Face> Faces { get; set; }
-        public IList<Campaign> Campaigns { get; set; }
+        public IList<Face> Faces { get; }
+        public IList<Campaign> Campaigns { get; }
 
-        public DataContext()
+        public DataContext(bool initialise = true)
         {
             Faces = new List<Face>();
             Campaigns = new List<Campaign>();
-            
-            InitializeData();
+
+            if (initialise)
+                InitializeData();
         }
 
         private void InitializeData()
@@ -24,17 +25,18 @@ namespace OohInterview.DAL
                 Id = Guid.NewGuid(),
                 Name = "First face"
             };
-            
+
             Faces.Add(firstFace);
-            
-            Campaigns.Add(new Campaign
-            {
-                Id = Guid.NewGuid(),
-                Name = "First Campaign",
-                StartDate = new DateTime(2019, 1, 1),
-                EndDate = new DateTime(2019, 12, 31),
-                Faces = new List<Face> { firstFace }
-            });
+
+            Campaigns.Add(
+                new Campaign
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "First Campaign",
+                    StartDate = new DateTime(2019, 1, 1),
+                    EndDate = new DateTime(2019, 12, 31),
+                    Faces = new List<Face> { firstFace }
+                });
         }
     }
 }
