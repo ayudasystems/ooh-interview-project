7fe6279536199ef5e38fdec9417559d0a330a0ea
Marc-André Beaudet-Racine <>
2020-02-11 15:10:18 -0500

    add table to show faces

--- OohInterview.UI.VanillaJS/inventory.js
+++ OohInterview.UI.VanillaJS/inventory.js
@@ -1,4 +1,4 @@
-function loadInitialUI(){
+function loadInitialUI() {
     loadFaces();
     setBookingDates();
 }
@@ -12,9 +12,12 @@ function loadFaces () {
             let data = JSON.parse(this.response);
             
             if(data.items.length > 0){
+                createFaceGrid();
                 data.items.forEach(face => {
-                    addFaceToUI(face);
+                    addFaceToGrid(face);
                 });
+            }else{
+                showNoFacesMessage();
             }
         }
     };
@@ -22,25 +25,99 @@ function loadFaces () {
     request.send();
 }
 
-function addFaceToUI(face){
-    let faceElement = createFaceElement(face);
-
-    addFaceElementToUI(faceElement);
+function createFaceGrid() {
+    const gridElement = document.createElement('table');
+    gridElement.id = "faces-grid";
+    
+    let headerElement = createHeader();
+    gridElement.appendChild(headerElement);
+    
+    let bodyElement = createBody();
+    gridElement.appendChild(bodyElement);
+    
+    addElementToFaceContainer(gridElement);
 }
 
-function createFaceElement(face){
-    const faceElement = document.createElement('div');
-    faceElement.textContent = face.name;
+function createHeader() {
+    const headElement = document.createElement('thead');
 
-    return faceElement;
+    let headerRow = createHeaderRow();
+    headElement.appendChild(headerRow);
+    
+    return headElement;
 }
 
-function addFaceElementToUI(faceElement){
+function createHeaderRow() {
+    const headRowElement = document.createElement('tr');
+
+    let faceNameHeader = createHeaderCell("Face Name");
+    headRowElement.appendChild(faceNameHeader);
+
+    let availabilityHeader = createHeaderCell("Availability");
+    headRowElement.appendChild(availabilityHeader);
+    
+    return headRowElement;
+}
+
+function createHeaderCell(text) {
+    const headerCellElement = document.createElement('th');
+    headerCellElement.textContent = text;
+    
+    return headerCellElement;
+}
+
+function createBody() {
+    const bodyElement = document.createElement('tbody');
+    bodyElement.id = "faces-grid-body";
+    
+    return bodyElement;
+}
+
+function showNoFacesMessage() {
+    let noFacesMessageElement = createNoFacesMessage();
+    addElementToFaceContainer(noFacesMessageElement);
+}
+
+function createNoFacesMessage() {
+    const noFacesMessageElement = document.createElement('div');
+    noFacesMessageElement.className = "no-faces-message";
+    noFacesMessageElement.textContent = "No faces found";
+
+    return noFacesMessageElement;
+}
+
+function addFaceToGrid(face) {
+    let faceRowElement = createFaceRow(face);
+    
+    const facesGridBody = document.getElementById('faces-grid-body');
+    facesGridBody.appendChild(faceRowElement);
+}
+
+function createFaceRow(face) {
+    const faceRowElement = document.createElement('tr');
+
+    let faceNameCell = createFaceCell(face.name);
+    faceRowElement.appendChild(faceNameCell);
+
+    let availabilityCell = createFaceCell("Not implemented yet");
+    faceRowElement.appendChild(availabilityCell);
+    
+    return faceRowElement;
+}
+
+function createFaceCell(text) {
+    const faceCellElement = document.createElement('td');
+    faceCellElement.textContent = text;
+
+    return faceCellElement;
+}
+
+function addElementToFaceContainer(element) {
     const facesContainer = document.getElementById('faces-container');
-    facesContainer.appendChild(faceElement);
+    facesContainer.appendChild(element);
 }
 
-function setBookingDates(){
+function setBookingDates() {
     let today = new Date();
     let todayString = getDateString(
         today.getDate(),
@@ -52,7 +129,7 @@ function setBookingDates(){
     document.getElementById('end-date').value = todayString;
 }
 
-function getDateString(day, month, year){
+function getDateString(day, month, year) {
     if (month < 10) month = "0" + month;
     if (day < 10) day = "0" + day;
 
