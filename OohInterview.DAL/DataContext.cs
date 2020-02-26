bdb7b15842d15a862aaefd61e04237d551b95e0d
Marc-André Beaudet-Racine <>
2020-02-26 12:11:00 -0500

    add more seed data

--- OohInterview.DAL/DataContext.cs
+++ OohInterview.DAL/DataContext.cs
@@ -23,11 +23,25 @@ namespace OohInterview.DAL
 
         private void InitializeData()
         {
-            var firstFace = new Face()
+            var firstFace = new Face
             {
                 Id = Guid.NewGuid(),
                 Name = "First face",
                 RatePerDay = 5212.67m
+            };
+            
+            var secondFace = new Face
+            {
+                Id = Guid.NewGuid(),
+                Name = "Second face",
+                RatePerDay = 4572.12m
+            };
+            
+            var availableFace = new Face
+            {
+                Id = Guid.NewGuid(),
+                Name = "Available face",
+                RatePerDay = 123.45m
             };
 
             var firstCampaign = new Campaign
@@ -37,16 +51,42 @@ namespace OohInterview.DAL
                 StartDate = new DateTime(2019, 1, 1),
                 EndDate = new DateTime(2019, 12, 31),
             };
+            
+            var campaignWith2Bookings = new Campaign
+            {
+                Id = Guid.NewGuid(),
+                Name = "Campaign with 2 bookings",
+                StartDate = new DateTime(2020, 1, 1),
+                EndDate = new DateTime(2020, 10, 31),
+            };
 
-            var firstBooking = new Booking()
+            var booking1 = new Booking
             {
                 CampaignId = firstCampaign.Id,
                 FaceId = firstFace.Id
             };
 
+            var booking2 = new Booking
+            {
+                CampaignId = campaignWith2Bookings.Id,
+                FaceId = firstFace.Id
+            };
+
+            var booking3 = new Booking
+            {
+                CampaignId = campaignWith2Bookings.Id,
+                FaceId = secondFace.Id
+            };
+            
+
             Faces.Add(firstFace);
+            Faces.Add(secondFace);
+            Faces.Add(availableFace);
             Campaigns.Add(firstCampaign);
-            Bookings.Add(firstBooking);
+            Campaigns.Add(campaignWith2Bookings);
+            Bookings.Add(booking1);
+            Bookings.Add(booking2);
+            Bookings.Add(booking3);
         }
     }
 }
